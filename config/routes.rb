ff5441e9dd76ca396bf5d5e1d9f67a5e9ee24b2d
Gregory Dombchik <gbdjobs1@gmail.com>
2019-07-02 21:58:40 -0400

    add initial pages of the article resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#goodbye'
-  #root 'welcome#index'
+  get 'welcome/index'
 
   resources :articles
+
+  root 'application#hello'
+  root 'welcome#index'
+
+
 end
